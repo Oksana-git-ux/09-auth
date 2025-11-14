4bd72efc45038931e6220724dd0826b02e2879a5
Oksana_Melnyk <kc.19870625@gmail.com>
2025-11-14 12:01:43 +0200

    update files app/(private routes)/notes/filter/[...slug]/Notes.client.tsx and page.tsx

--- app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
+++ app/(private routes)/notes/filter/[...slug]/Notes.client.tsx
@@ -1,80 +1,67 @@
-"use client";
+"use client"
 
 
-import css from "./NotesPage.module.css";
-import NoteList from "@/components/NoteList/NoteList";
-import { fetchNotes } from "@/lib/api/clientApi";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import SearchBox from "@/components/SearchBox/SearchBox";
-import Pagination from "@/components/Pagination/Pagination";
-import { useDebouncedCallback } from "use-debounce";
-import Loader from "@/components/Loader/Loader";
-import { keepPreviousData } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-
+import css from './NotesPage.module.css'
+import NoteList from '@/components/NoteList/NoteList'
+import { fetchNotes } from '@/lib/api/clientApi'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import SearchBox from '@/components/SearchBox/SearchBox'
+import Pagination from '@/components/Pagination/Pagination'
+import { useDebouncedCallback } from 'use-debounce'
+import Loader from '@/components/Loader/Loader'
+import { keepPreviousData } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 
 interface Props {
-tag?: string;
+    tag?: string
 }
 
-
 export default function NotesClient({ tag }: Props) {
-const safeTag = tag ?? undefined;
+    const [page, setPage] = useState(1)
+    const [searchValue, setSearchValue] = useState('')
+    const [inputValue, setInputValue] = useState('')
+
+    const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value
+        setInputValue(value)
+        handleSearch(value)
+    }
+    const handleSearch = useDebouncedCallback((value) => {
+        setSearchValue(value)
+        setPage(1)
+    }, 700)
 
 
-const [page, setPage] = useState(1);
-const [searchValue, setSearchValue] = useState("");
-const [inputValue, setInputValue] = useState("");
+    const { data, isFetching, } = useQuery({
+        queryKey: ['notes', page, searchValue, tag],
+        queryFn: () => fetchNotes(page, 12, searchValue, tag),
+        refetchOnMount: false,
+        placeholderData: keepPreviousData
 
 
-const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-const value = e.target.value;
-setInputValue(value);
-handleSearch(value);
-};
+    })
+    const router = useRouter()
+    return (
+        <div className={css.app}>
+            <header className={css.toolbar}>
 
+                <SearchBox value={inputValue} onChange={handleInputValue} />
+                {data && data.totalPages > 1 && <Pagination totalPages={data.totalPages} page={page} setPage={setPage} />}
 
-const handleSearch = useDebouncedCallback((value) => {
-setSearchValue(value);
-setPage(1);
-}, 700);
+                <button className={css.button} onClick={() => { router.push('/notes/action/create') }}>Create note +</button>
 
+            </header>
 
-const { data, isFetching } = useQuery({
-queryKey: ["notes", page, searchValue, safeTag],
-queryFn: () => fetchNotes(page, 12, searchValue, safeTag),
-refetchOnMount: false,
-placeholderData: keepPreviousData,
-});
+            {isFetching && <Loader />}
 
+            {data && data.notes.length > 0 ? (
+                <NoteList notes={data.notes} />
+            ) : (
+                <p>Empty...</p>
+            )}
 
-const router = useRouter();
+        </div>
 
-
-const notes = data?.notes ?? [];
-const totalPages = data?.totalPages ?? 1;
-
-
-return (
-<div className={css.app}>
-<header className={css.toolbar}>
-<SearchBox value={inputValue} onChange={handleInputValue} />
-
-
-{totalPages > 1 && <Pagination totalPages={totalPages} page={page} setPage={setPage} />}
-
-
-<button className={css.button} onClick={() => router.push("/notes/action/create")}>
-Create note +
-</button>
-</header>
-
-
-{isFetching && <Loader />}
-
-
-{notes.length > 0 ? <NoteList notes={notes} /> : <p>Empty...</p>}
-</div>
-);
+    )
 }
